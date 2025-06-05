91be0a20d6587c459348cf6d0ba34a0e50e56e77
MESCIUS JP Developer Solutions Marketing <48461005+MESCIUSJPDevloperSolutions@users.noreply.github.com>
2025-06-05 17:28:42 +0900

    Update app.js

--- spreadjs-reportsheet-excel-report-import/scripts/app.js
+++ spreadjs-reportsheet-excel-report-import/scripts/app.js
@@ -29,8 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 reportSheet = spread.getSheetTab(0);
 
                 // レポートシートのオプション設定
-                reportSheet.options.renderMode = 'PaginatedPreview';
-                //reportSheet.options.renderMode = 'Preview';
+                reportSheet.renderMode('PaginatedPreview');
                 reportSheet.options.printAllPages = true;
 
                 // レポートシートの印刷設定
@@ -38,7 +37,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 printInfo.showBorder(false);
                 printInfo.zoomFactor(1);
                 reportSheet.printInfo(printInfo);
-                reportSheet.refresh();
                 initPage()
             }, (e) => {
                 console.log(`***ERR*** エラーコード（${e.errorCode}） : ${e.errorMessage}`);
